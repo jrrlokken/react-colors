554a81eead332162d50810875eefe6e013bde32b
JOLOK <50845333+jrrlokken@users.noreply.github.com>
2020-01-05 12:22:06 -0600

    Add Navbar and PaletteFooter to SingleColorPalette

--- src/Navbar.js
+++ src/Navbar.js
@@ -24,13 +24,14 @@ class Navbar extends Component {
     this.setState({ open: false });
   }
   render() {
-    const { level, changeLevel } = this.props;
+    const { level, changeLevel, showSlider } = this.props;
     const { format } = this.state;
     return (
       <header className='Navbar'>
         <div className='logo'>
           <Link to='/'>PaletteSwatch</Link>
         </div>
+        {showSlider && (
         <div className='slider-container'>
           <span>Level: {level}</span>
           <div className='slider'>
@@ -43,6 +44,7 @@ class Navbar extends Component {
             />
           </div>
         </div>
+        )}
         <div className='select-container'>
           <Select value={format} onChange={this.handleFormatChange}>
             <MenuItem value='hex'>HEX - #FFFFFF</MenuItem>
