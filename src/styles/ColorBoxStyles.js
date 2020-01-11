ad0f8810abfc3f594498fb56b0d7bfa42c6581c2
JOLOK <50845333+jrrlokken@users.noreply.github.com>
2020-01-11 13:38:36 -0600

    Make ColorBox responsive

--- src/styles/ColorBoxStyles.js
+++ src/styles/ColorBoxStyles.js
@@ -16,11 +16,11 @@ export default {
     },
     [sizes.down('lg')]: {
       width: '25%',
-      height: props => (props.showFullPalette ? '20%' : '50%'),
+      height: props => (props.showFullPalette ? '20%' : '33.3333%'),
     },
     [sizes.down('md')]: {
       width: '50%',
-      height: props => (props.showFullPalette ? '10%' : '50%'),
+      height: props => (props.showFullPalette ? '10%' : '20%'),
     },
     [sizes.down('xs')]: {
       width: '100%',
@@ -114,6 +114,9 @@ export default {
       textAlign: 'center',
       marginBottom: '0',
       padding: '1rem',
+      [sizes.down('xs')]: {
+        fontSize: '5rem'
+      }
     },
     '& p': {
       fontSize: '2rem',
